a2a1295552d80ee4b3ad8bd4c3af3d3c94d0972b
BernardoJFilho <bernardojcm.filho@gmail.com>
2022-11-14 09:48:02 -0300

    adiciona os arquivos

---
+++ calculadora.js
@@ -0,0 +1,17 @@
+function insert(parara) {
+  var numero = document.getElementsByid("resultado").innerHTML;
+  document.getElementById("resultado").innerHTML = numero + parara;
+}
+function clean
+{
+    document.getElementById('resultado').innerHTML = "";
+}
+function back
+{
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado')
+}
+function calcular
+{
+    
+}
